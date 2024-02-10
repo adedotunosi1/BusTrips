ae59cd134f5bd130e00ca523a0920d9384678f8d
adedotunosi1 <osiefaadedotun@gmail.com>
2024-02-10 18:10:12 +0100

    Bus Data

--- src/controllers/busController.js
+++ src/controllers/busController.js
@@ -1,8 +1,10 @@
 const busTripUsers = require('../models/AfriMoveUsersModel');
 const { addNewBus } = require('../services');
 const allAfriMoveBus = require('../models/AfriMoveBusModel');
-
+const fs = require('fs');
+const path = require('path');
 const { cloudinary, uploadToCloudinary } = require('../utils/cloudinary');
+const mongoose = require('mongoose')
 
 const add_bus = async (req, res, next) => {
     const { plateNumber, manufacturer, model, year, capacity, ratings, destination, busStatus, duration, distance } = req.body;
@@ -43,7 +45,7 @@ const get_bus = async (req, res, next) => {
         
         try {
          const afriMoveBus = await allAfriMoveBus.find({ });
-
+          
            res.json({ message: 'Your AfriMove Bus', data: afriMoveBus });
 
         } catch (error) {
@@ -53,25 +55,85 @@ const get_bus = async (req, res, next) => {
 }
 
 const bus_image = async (req, res) => {
-    try {
-        const busId = req.body;
-        const imageUrl = req.file; // Cloudinary URL of the uploaded image
-    
-        // Update the bus document with the Cloudinary image URL
-        const updatedBus = await Bus.findByIdAndUpdate(
-          busId,
-          { busImage: imageUrl },
-          { new: true }
-        );
-    
-        res.json(updatedBus);
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Failed to upload image' });
+  const { base64, plateNumber } = req.body;
+  console.log(plateNumber);
+  try {
+
+    // Find bus by busId
+    const bus = await allAfriMoveBus.findOne({ plateNumber })
+    console.log(bus)
+    if (!bus) {
+      return res.status(404).json({ error: 'Bus not found.' });
+    }
+
+    if (!base64) {
+      return res.status(400).json({ error: 'Missing required parameter - file' });
+    }
+
+    // Convert base64 image data to a buffer
+    const imageBuffer = Buffer.from(base64, 'base64');
+
+    // Create the temp directory if it doesn't exist
+    const tempDirPath = path.join(__dirname, '..', 'temp');
+    if (!fs.existsSync(tempDirPath)) {
+      fs.mkdirSync(tempDirPath);
+    }
+
+    // Create a temporary file path for the image
+    const tempImagePath = path.join(tempDirPath, `${plateNumber}-temp-image.jpg`);
+
+    // Write the buffer to the temporary file
+    fs.writeFileSync(tempImagePath, imageBuffer);
+
+    // Upload image to cloudinary
+    cloudinary.uploader.upload(tempImagePath, async (error, result) => {
+      // Delete the temporary file
+      fs.unlinkSync(tempImagePath);
+
+      if (error) {
+        console.log(error);
+        return res.send({ status: 'error', data: error });
       }
+      console.log(result.secure_url);
+      try {
+        // Update busImage field and save the document
+        bus.busImage = result.secure_url;
+        await bus.save();
+
+        res.send({ status: 'ok', message: 'Bus Image upload successful', data: result.secure_url });
+      } catch (error) {
+        console.log(error);
+        res.send({ status: 'error', data: error });
+      } 
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ status: 'error', data: error });
+  }
 }
+
+const bus_data = async (req, res) => {
+  const { busId } = req.body
+  try {
+    if (!busId) {
+      return res.status(404).json({ error: 'Bus Id needed' });
+    }
+    const afriMoveBus = await allAfriMoveBus.find({busId});
+    if (!afriMoveBus) {
+      return res.status(404).json({ error: 'Bus not found.' });
+    }
+      res.json({ message: 'Your Bus Data', data: afriMoveBus });
+
+   } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error", codeerror: error });
+   }
+}
+
 
 module.exports = {
     add_bus,
-    get_bus
+    get_bus,
+    bus_image,
+    bus_data
 } 
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,9 +6,12 @@ const { requireUser } = require('../middlewares/auth.middleware');
 const userRoute = express.Router();
 
 userRoute.post('/imageUpload', requireUser, upload.single('profileImage'), Controller.UserController.userImage);
-userRoute.get('/data', requireUser, Controller.UserController.userData);
+userRoute.get('/data', Controller.UserController.userData);
 userRoute.post('/add-bus', requireUser, upload.single('image'), Controller.BusController.add_bus);
 userRoute.get('/bus', requireUser, Controller.BusController.get_bus);
+userRoute.post('/bus-image', upload.single('image'), Controller.BusController.bus_image);
+userRoute.post('/bus-data', requireUser, Controller.BusController.bus_data);
+
 module.exports = {
     userRoute,
 }
